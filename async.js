1f03563c5ff50db6e58fc52dce4f855b950a468d
erobinofficial <erobin8778@gmail.com>
2023-01-22 00:11:14 +0600

    aaa

--- async.js
+++ async.js
@@ -2,8 +2,9 @@ const loadUsers = () => {
     const url = `https://randomuser.me/api/?gender=female`;
     fetch(url)
     .then(res => res.json())
-    .then(data = console.log(data));
+    .then(data = console.log(data.results))
 }
-// const displayUsers = user => {
-//     console.log(user);
-// }
+
+const displayUsers = user => {
+    console.log(user);
+}
