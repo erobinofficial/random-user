3eef16e428bd757cb3dbb9ff1964b51cb9b49d6c
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2023-01-21 18:00:47 +0000

    fixed

--- async.js
+++ async.js
@@ -1,9 +1,9 @@
 const loadUsers = () => {
     const url = `https://randomuser.me/api/?gender=female`;
     fetch(url)
-    .then(res => res.json())
-    .then(data = console.log(data));
+        .then(res => res.json())
+        .then(data => displayUsers(data.results))
 }
-// const displayUsers = user => {
-//     console.log(user);
-// }
+const displayUsers = user => {
+    console.log(user);
+}
