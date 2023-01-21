3401c75c8421bec10abe1cf5428720685f176b7f
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2023-01-21 18:01:56 +0000

    fixed:single user data

--- async.js
+++ async.js
@@ -2,7 +2,7 @@ const loadUsers = () => {
     const url = `https://randomuser.me/api/?gender=female`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayUsers(data.results))
+        .then(data => displayUsers(data.results[0]))
 }
 const displayUsers = user => {
     console.log(user);
